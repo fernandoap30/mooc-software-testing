515208f70e28fdf23b126d9ad8ed3b6d5ee54f4f
fernandoap30 <fernando.apmarce22@gmail.com>
2025-03-28 18:46:43 -0500

    test: se agregaron los 3 testeos de la guia de laboratorio con satisfacción

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,24 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    //asertTrue and assertFalse
+    @Test
+    void testAllHappy() {
+        GHappy gh = new GHappy();
+        Assertions.assertTrue(gh.gHappy("xxggxx"));
+    }
+
+    @Test
+    void testSingleG() {
+        GHappy gh = new GHappy();
+        Assertions.assertFalse(gh.gHappy("xxgxx"));
+    }
+    @Test
+    void testSeveralGs() {
+        GHappy gh = new GHappy();
+        Assertions.assertFalse(gh.gHappy("xxggyygxx"));
+    }
 }
