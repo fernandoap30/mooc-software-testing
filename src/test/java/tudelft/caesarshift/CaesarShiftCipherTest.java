fd4571d195dcb9c445dd9a4bccbfe391c322535f
fernandoap30 <fernando.apmarce22@gmail.com>
2025-03-28 19:55:13 -0500

    test: se agregaron 3 testeos a la funcion CaesarShiftCipher para corroborar casos normales, casos con espacios y caso con letras finales como la z

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,25 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CaesarShiftCipherTest {
+
+    @Test
+    void test1() {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        assertEquals("efg", csc.CaesarShiftCipher("abc", 4));
+    }
+    @Test
+    void test2() {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        assertEquals("cde cde", csc.CaesarShiftCipher("abc abc", 2));
+    }
+
+    @Test
+    void test3() {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        assertEquals("def dac", csc.CaesarShiftCipher("abc axz", 3));
+    }
 
 }
